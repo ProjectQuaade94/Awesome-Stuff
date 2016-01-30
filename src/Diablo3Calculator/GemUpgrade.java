2e253697734c92a7d02526acef0a04ec45e1a5f4
Quaade94 <larsquaadeconnect@gmail.com>
2016-01-30 15:23:44 +0100

    ryder op i program

--- src/Diablo3Calculator/GemUpgrade.java
+++ src/Diablo3Calculator/GemUpgrade.java
@@ -4,40 +4,73 @@ import javax.swing.JOptionPane;
 
 public class GemUpgrade {
 
-	private double griftLevel;
-	private double gemLevel;
+	private int gemLevel;
 	private String input;
 	private boolean loop = true;
 	private int response = 0;
-	private String[] choices = {"Enter Gem level", "Enter Grift level", "Back"};
+	private String[] choices = {"Enter new Gem level", "Back"};
 	private String message;
-	
+	private String[] chance = {null,null,null,null,null,null,null,null,null,null,null,null};
+	private String chances;
+
 	public void gemUpgrade(){
-			while (loop) {
-				message = "Grift level: "+(int)griftLevel+"\nGem Level: "+(int)gemLevel+"\n\nEnter Gem and/or Grift level:";
-				response = JOptionPane.showOptionDialog(
-						null                         // Center in window.
-						, message        // Message
-						, "Diablo 3 Calculator"      // Title in titlebar
-						, JOptionPane.YES_NO_OPTION  // Option type
-						, JOptionPane.PLAIN_MESSAGE  // messageType
-						, null                       // Icon (none)
-						, choices                    // Button text as above.
-						, ""    // Default button's label
-						);
-				if (response == 0){
+		while (loop) {
+			if (input == null){
+				while(gemLevel<1){
 					input = JOptionPane.showInputDialog(null, "Enter Gem level:");
-					gemLevel = Double.parseDouble(input);
-				}
-				if (response == 1){
-					input = JOptionPane.showInputDialog(null, "Enter Grift level:");
-					griftLevel = Double.parseDouble(input);
-				}
-				if (response == 2){
-					break;
+					gemLevel = Integer.parseInt(input);
+					if(gemLevel<1){
+						JOptionPane.showMessageDialog(null,"The gem must be level 1 or above!");
+					}
 				}
 			}
 
+			int[] negCheck = {gemLevel+12,gemLevel+11,gemLevel+10,gemLevel+9,gemLevel+8,gemLevel-1,gemLevel-2,gemLevel-3,gemLevel-4,gemLevel-5,gemLevel-6};
+
+			chance[0] = "\n100% at "+negCheck[0]+" and above";
+			chance[1] = "\n90% at "+negCheck[1];
+			chance[2] = "\n80% at "+negCheck[2];
+			chance[3] = "\n70% at "+negCheck[3];
+			chance[4] = "\n60% between levels "+gemLevel+" to "+negCheck[4];
+			chance[5] = "\n30% at "+negCheck[5];
+			chance[6] = "\n15% at "+negCheck[6];
+			chance[7] = "\n8% at "+negCheck[7];
+			chance[8] = "\n4% at "+negCheck[8];
+			chance[9] = "\n2% at "+negCheck[9];
+			chance[10] = "\n1% at "+negCheck[10]+" and below, until and including "+(gemLevel-15);
+
+			for(int i=0 ; i<11 ; i++){
+				String adder = "";
+				if(gemLevel>0 && negCheck[i]>0){
+
+					adder = chance[i];
+				}
+				chances = chances + (adder+"\n");
+			}
+
+
+			message = "\nGem Level: "+gemLevel+ "\n"+"\nYour chances for an upgrade are as follows:\n" +chances+ "\n\nEnter Gem and/or Grift level:";
+			response = JOptionPane.showOptionDialog(
+					null                         // Center in window.
+					, message        // Message
+					, "Diablo 3 Calculator"      // Title in titlebar
+					, JOptionPane.YES_NO_OPTION  // Option type
+					, JOptionPane.PLAIN_MESSAGE  // messageType
+					, null                       // Icon (none)
+					, choices                    // Button text as above.
+					, ""    // Default button's label
+					);
+			if (response == 0){
+				input = JOptionPane.showInputDialog(null, "Enter new Gem level:");
+				gemLevel = Integer.parseInt(input);
+			}
+			if (response == 1){
+				break;
+			}
+		}
+	}
 }
-	
-}
+
+
+
+
--- src/Diablo3Calculator/Main.java
+++ src/Diablo3Calculator/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		  JFrame window = new JFrame();                           //Note 2
-	        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //Note 3
+		  JFrame window = new JFrame();
+	        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        window.setVisible(true);   
 		
 		Controller C = new Controller();
