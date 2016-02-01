8087735a58a91d2214e09d7f219fa3e5a0410c61
Quaade94 <larsquaadeconnect@gmail.com>
2016-02-01 12:59:48 +0100

    scapelon

--- src/Diablo3Calculator/Builds.java
+++ src/Diablo3Calculator/Builds.java
@@ -1,9 +1,25 @@
 package Diablo3Calculator;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
 public class Builds {
 
+	private Object[] classNames = {"Crusader", "Demon Hunter", "Witch Doctor", "Monk", "Wizard", "Barbarian"};
+	
+	private Object[][] filler = {};
+	
 	public void builds(){
-		
+		JFrame window = new JFrame();
+		JTable table = new JTable(filler, classNames);
+		JScrollPane scrollPane = new JScrollPane(table);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.add(scrollPane, BorderLayout.CENTER);
+		window.setSize(1200, 220);
+		window.setVisible(true);
 	}
 	
 }
