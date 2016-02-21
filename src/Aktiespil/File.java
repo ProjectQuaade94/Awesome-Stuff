317ca57720e5facb8a7f75310157ac013a80da92
Quaade94 <Quaade94@Quaade94-Pc>
2016-02-21 02:29:51 +0100

    kommit

--- src/Aktiespil/File.java
+++ src/Aktiespil/File.java
@@ -10,45 +10,78 @@ public class File {
 
 	private int data;
 	private String isEmpty;
-	
+
 	public void setData(int data){
 
 		try {
 			BufferedWriter writer = 
 					new BufferedWriter ( new FileWriter(".\\Data.txt"));
-			
+
 			writer.write(""+data);
 			writer.close();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public int getData(){
-		
+
 		try {
-		BufferedReader reader =
-				new BufferedReader (new FileReader (".\\Data.txt"));
-		
-		isEmpty = reader.readLine();
-		data = Integer.parseInt(reader.readLine());
-		reader.close();
-		
-		
+			BufferedReader reader =
+					new BufferedReader (new FileReader (".\\Data.txt"));
+
+			isEmpty = reader.readLine();
+			data = Integer.parseInt(reader.readLine());
+			reader.close();
+
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return data;
 	}
-	
+
 	public boolean isEmpty(){
 		if(isEmpty==null){
-		return true;
+			return true;
 		}
 		else{
 			return false;
 		}
 	}
-	
+	private boolean dream;
+	private int goals;
+	private int complete;
+
+
+
+																		public void dreamer(){
+
+																			while(dream != true){
+																				goals++;
+																				
+																				if(goals == complete){
+																					dream = true;
+																				}
+																			}
+																		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
