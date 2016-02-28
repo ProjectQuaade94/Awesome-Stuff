d506a11bf78ccd9ea0bf8415bf385c4f7ad3d85a
Quaade94 <larsquaadeconnect@gmail.com>
2016-02-28 16:30:45 +0100

    mee til alg

--- Flere opgaver/Main.java
+++ Flere opgaver/Main.java
@@ -1,16 +1,26 @@
 
 
 public class Main {
-	
 	public static void main(String[] args) {
-
 		int n = 5000;
-		
-		
-		Alg3 Alg3 = new Alg3();
-		
-		Alg3.alg3(n);
-		
+
+		alg3(n);
 	}
 
+	public static void alg3(int n){
+		long startTid = System.currentTimeMillis()/1000;
+		while(true){
+			for(int i = 1; i <= n; i++){
+				for(int j = 1; j <= n; j++){
+					int k=j;
+					while(k<=n){
+						k=k*3;
+					}
+				}
+			}
+			long slutTid = System.currentTimeMillis()/1000;
+			System.out.println("n = " + n + " Tid i min: " + ((slutTid-startTid)/60));
+			n = n*2;
+		}
+	}
 }
