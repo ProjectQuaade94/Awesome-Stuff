95d9d08eb3c7d697e9811670590ad2ee83e47859
Quaade94 <larsquaadeconnect@gmail.com>
2016-03-02 10:46:14 +0100

    alg

--- Flere opgaver/Main.java
+++ Flere opgaver/Main.java
@@ -2,13 +2,14 @@
 
 public class Main {
 	public static void main(String[] args) {
-		int n = 5000;
+		int n = 50000;
 
 		alg3(n);
 	}
 
 	public static void alg3(int n){
 		long startTid = System.currentTimeMillis()/1000;
+		long lastTime=-1;
 		while(true){
 			for(int i = 1; i <= n; i++){
 				for(int j = 1; j <= n; j++){
@@ -19,8 +20,12 @@ public class Main {
 				}
 			}
 			long slutTid = System.currentTimeMillis()/1000;
-			System.out.println("n = " + n + " Tid i min: " + ((slutTid-startTid)/60));
+			long t = (slutTid-startTid);
+			long ratio = t/lastTime;
+			lastTime = t;
+			System.out.println("n = " + n + " Tid i sek: " + t + " ratio: " +ratio);
 			n = n*2;
+			
 		}
 	}
 }
